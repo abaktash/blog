4624370a5c2d969191925c73b11c97134ff61329
abaktash <ali@ckosv.com>
2021-08-22 19:06:59 -0700

    Forms Half Way

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -10,4 +11,8 @@ class Post(models.Model):
 
     def __str__(self):   # what the string that is printed with object call
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[str(self.id)]) # the id works as pk 
+    
         
